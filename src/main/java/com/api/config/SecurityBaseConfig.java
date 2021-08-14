c5640793c048ddc4c05403ce8219d1de75fa5154
Jadson Feitosa <jadson.feitosa.silva@gmail.com>
2021-08-14 19:14:41 -0300

    #39 - -Create user of crud from api - desenvolido metodo de login criptografia de senha

--- src/main/java/com/api/config/SecurityBaseConfig.java
+++ src/main/java/com/api/config/SecurityBaseConfig.java
@@ -1,21 +1,29 @@
 package com.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityBaseConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsImpl userDetailsService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 		.withUser("admin").password("Ads%$#@!Ads").roles("ROEL_ADMIN");
+		
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	@Override
