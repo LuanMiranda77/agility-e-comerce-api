409df803084ab98df573bede44067a9fbca0de1f
Jadson Feitosa <jadson.feitosa.silva@gmail.com>
2021-10-02 15:40:47 -0300

    Correções e crud de endereco AE-43

--- src/main/java/com/api/domain/Usuario.java
+++ src/main/java/com/api/domain/Usuario.java
@@ -5,9 +5,12 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
 
 import com.sun.istack.NotNull;
@@ -42,14 +45,16 @@ public class Usuario {
 	@NotNull
 	@Size(min = 6)
 	private String password;
+	
+	@NotNull
+	@Enumerated(EnumType.ORDINAL)
+	private Roles role;
 
 	
 	public void dataInicial() {
 		this.dataCriacao = new Timestamp(System.currentTimeMillis());
 		this.dataAtualizacao = new Timestamp(System.currentTimeMillis());
 	}
-	
-
 	
 	
 }
