e8ff70d181b5dc6c74b7ac1ad07193953ac19d30
LuanMiranda77 <luanprof30@gmail.com>
2021-11-02 21:05:34 -0300

    Correção de teste em pedido

--- src/main/java/com/api/domain/Pagamento.java
+++ src/main/java/com/api/domain/Pagamento.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -42,9 +44,11 @@ public class Pagamento implements Serializable {
 	private Date dataPagamento;
 	
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	private TipoPagamento tipo;
 	
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	private EstatusPagamento estatus;
 	
 	
