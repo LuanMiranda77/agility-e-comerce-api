0acafa16ef5ee461c8c7ff5c0ebc7354cdb7e714
LuanMiranda77 <luanprof30@gmail.com>
2022-02-05 13:38:17 -0300

    configuração em produção

--- src/main/java/com/api/config/SecurityOauth2.java
+++ src/main/java/com/api/config/SecurityOauth2.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //@EnableWebSecurity
 //@EnableAuthorizationServer
 //@EnableResourceServer
-@Profile("prod")
+@Profile("dev")
 public class SecurityOauth2 extends WebSecurityConfigurerAdapter{
 	
 	@Bean
